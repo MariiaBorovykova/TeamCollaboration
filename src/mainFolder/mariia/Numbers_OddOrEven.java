42057ffdd00a0d956dbe594b94e7b032f8d7422f
Mariia <mashabobrakova@gmail.com>
2024-01-28 23:08:22 -0500

    create class Odd or Even numbers

---
+++ src/mainFolder/mariia/Numbers_OddOrEven.java
@@ -0,0 +1,19 @@
+package mainFolder.mariia;
+
+public class Numbers_OddOrEven {
+
+    public static void main(String[] args) {
+        odOrEven(10);
+        odOrEven(11);
+    }
+
+    public static void odOrEven(int n){
+
+        if (n % 2 == 0 ){
+            System.out.println(n + " is even number");;
+        } else {
+            System.out.println(n + " is odd number");
+        }
+
+    }
+}

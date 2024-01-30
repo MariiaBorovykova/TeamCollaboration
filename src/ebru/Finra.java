04ae7ac184b979a579276b9cde150863871177e6
EbruErol <ebrukaragozler@icloud.com>
2024-01-30 00:10:45 -0500

    CreateFinra

---
+++ src/ebru/Finra.java
@@ -0,0 +1,28 @@
+package ebru;
+
+public class Finra {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else {
+                System.out.print(i + " ");
+            }
+
+        }
+
+        System.out.println();
+
+
+    }
+
+}

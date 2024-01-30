53f2fbca24019f0e88b5ad78f57899da39ba8d1e
merve.erdogan <mrverdogan7@gmail.com>
2024-01-30 01:42:32 -0500

    Create Odd Or Even

---
+++ src/Merve/OddOrEven.java
@@ -0,0 +1,30 @@
+package Merve;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        EvenOdd(5);
+        EvenOdd(6);
+
+    }
+
+
+    public static void EvenOdd(int number) {
+        if (number % 2 == 0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+
+}
+/*
+
+
+Write  a method which can identify given number is even or odd
+*Output ex:
+identify(5); ->"Odd"
+identify(6); ->"Even"*
+
+ */

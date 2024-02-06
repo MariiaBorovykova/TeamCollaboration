7b7e2cf862849c3dd18f01464776aed4c258932b
merverdogan <mrverdogan7@gmail.com>
2024-02-06 00:44:25 -0500

    create swap numbers

---
+++ src/Merve/week2/SwapNumbers.java
@@ -0,0 +1,23 @@
+package Merve.week2;
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y= 10;
+
+        x = x + y;   // x=5+10  x=15
+        y= x - y;    // y=15-10 y=5
+        x= x-y;     //  x=15-5  x=10;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+    }
+
+
+}
+/*
+Numbers -- Swap Numbers
+Swap two variable values without using a third variable
+ */
